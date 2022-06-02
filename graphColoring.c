470d7c9dbbceb88773557a9d7a2ae8f9dea87130
Yusuf Kaya <kayayusuf0044@hotmail.com>
2022-06-02 21:22:21 +0300

    graphcoloring version1

--- graphColoring.c
+++ graphColoring.c
@@ -1,15 +1,15 @@
 //Graph Coloring Problem
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
  
-
 struct AdjListNode {
     int dest;
     struct AdjListNode* next;
 };
  
 struct AdjList {
+    int color;
     struct AdjListNode* head;
 };
  
@@ -18,99 +18,153 @@ struct Graph {
     struct AdjList* array;
 };
  
-struct AdjListNode* newAdjListNode(int dest)
-{
-    struct AdjListNode* newNode = (struct AdjListNode*)malloc(sizeof(struct AdjListNode));
+struct AdjListNode* newAdjListNode(int dest) {
+    struct AdjListNode* newNode= (struct AdjListNode*)malloc(sizeof(struct AdjListNode));
     newNode->dest = dest;
     newNode->next = NULL;
     return newNode;
 }
  
-struct Graph* createGraph(int V)
-{
-    struct Graph* graph= (struct Graph*)malloc(sizeof(struct Graph));
+struct Graph* createGraph(int V) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
     graph->V = V;
- 
     graph->array = (struct AdjList*)malloc(V * sizeof(struct AdjList));
  
-    
     int i;
-    for (i = 0; i < V; ++i)
+    for (i = 0; i < V; ++i){
         graph->array[i].head = NULL;
- 
+        graph->array[i].color = 0;
+    }
     return graph;
 }
- 
+
 void addEdge(struct Graph* graph, int src, int dest)
 {
-
-    struct AdjListNode* check = NULL;
+ 
+    struct AdjListNode* temp = NULL;
     struct AdjListNode* newNode = newAdjListNode(dest);
- 
-    if (graph->array[src].head == NULL) {
-        newNode->next = graph->array[src].head;
-        graph->array[src].head = newNode;
-    }
-    else {
- 
-        check = graph->array[src].head;
-        while (check->next != NULL) {
-            check = check->next;
-        }
+    int isExist = 0;
+    
+    temp = graph->array[src].head;
+    while(temp){
+    	if(temp->dest==dest){isExist = 1;}
+    	temp = temp->next;
+	}
+	
+    if(graph->array[src].head != NULL && isExist==1){
+    	isExist=1;	
+	}
+    
 
-        check->next = newNode;
-    }
- 
-    newNode = newAdjListNode(src);
-    if (graph->array[dest].head == NULL) {
-        newNode->next = graph->array[dest].head;
-        graph->array[dest].head = newNode;
-    }
-    else {
-        check = graph->array[dest].head;
-        while (check->next != NULL) {
-            check = check->next;
-        }
-        check->next = newNode;
-    }
- 
-
+    else{
+    	if (graph->array[src].head == NULL) {
+	        graph->array[src].head = newNode;
+	    }
+	    else {
+	        temp = graph->array[src].head;
+	        while (temp->next != NULL) {
+	            temp = temp->next;
+	        }
+	        temp->next = newNode;
+	    } 
+	
+	    newNode = newAdjListNode(src);
+	    if (graph->array[dest].head == NULL) {
+	        graph->array[dest].head = newNode;
+	    }
+	    else {
+	        temp = graph->array[dest].head;
+	        while (temp->next != NULL) {
+	            temp = temp->next;
+	        }
+	        temp->next = newNode;
+	    }
+	}
+    
 }
 
 void printGraph(struct Graph* graph)
 {
     int v;
     for (v = 0; v < graph->V; ++v) {
-        struct AdjListNode* pCrawl = graph->array[v].head;
-        printf("\n Adjacency list of vertex %d\n head ", v);
-        while (pCrawl) {
-            printf("-> %d", pCrawl->dest);
-            pCrawl = pCrawl->next;
+        struct AdjListNode* temp = graph->array[v].head;
+        printf("\n Vertex %d\n head ", v+1);
+        while (temp) {
+            printf("-> %d", 1+temp->dest);
+            temp = temp->next;
         }
         printf("\n");
     }
 }
+int coloringAlgorithm(struct Graph* graph){
+    int colorValue = 0;
+    int chromaticNumber = 0;
+    int i;
+    int k;
+    struct AdjListNode* temp;
+    
+    int results[graph->V];
+    for(i=0; i<graph->V; ++i){ results[i]=-1;}
+    results[0]=0;
+    
+    int isColored[graph->V];
+    for(i=0; i<graph->V; ++i){ isColored[i]=0;}
+    
+    
+    for(k=1; k<graph->V; ++k){
+        temp = graph->array[k].head;
+        while(temp){
+            if(results[temp->dest]!=-1){ isColored[results[temp->dest]]=1; }
+            temp = temp->next;
+        }
+        colorValue = 0;
+        while(colorValue<graph->V){
+            if(isColored[colorValue]==0){break;}
+            colorValue+=1;
+        }
+        results[k] = colorValue;
+        
+        for(i=0; i<graph->V; ++i){ isColored[i]=0;}
+    }
+    
+    for(i=0;i<graph->V;++i){
+        printf("%d ",results[i]);
+        if(results[i]>chromaticNumber){chromaticNumber = results[i];}
+    }
+    
+    return chromaticNumber;
+}
  
-
 int main()
 {
-	FILE *file;
-	char inst;
-	int totalVertices;
-	int totalEdges;
-	int sourVertice;
-	int destVertice;
-	file = fopen("sample.txt", "r");
-	fscanf(file,"%c %d %d",inst,totalVertices,totalEdges);
-    struct Graph* graph = createGraph(totalVertices);
-    while(!(feof(file))){
-    	fscanf(file,"%c %d %d",inst,sourVertice,destVertice);
-    	addEdge(graph,sourVertice,destVertice);
-	}
-	
-
+    FILE* file;
+    char fileName[100];
+    printf("Please enter the name of the input file :\n");
+	scanf("%s", fileName);
+	file = fopen(fileName, "r");
+    int srcVer;
+    int destVer;
+    int totalVer;
+    int totalEdge;
+    char inst[5];
+    int chromaticNumber;
     
-    printGraph(graph);
- 	scanf("%d",&totalVertices);
+    struct Graph* graph;
+    while(!feof(file)){
+        fscanf(file,"%s %d %d",inst,&srcVer,&destVer);
+        if(strcmp(inst,"p")==0){
+            totalEdge = destVer;
+            totalVer = srcVer;
+            graph = createGraph(totalVer);
+            continue;
+        }
+        addEdge(graph, srcVer-1, destVer-1);
+    }
+    //printGraph(graph);
+    
+    chromaticNumber = coloringAlgorithm(graph);
+    printf("\n%d", chromaticNumber);
+    fclose(file);
+    scanf("\n%d", &chromaticNumber);
     return 0;
 }
